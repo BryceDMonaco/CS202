fe855c535108a1e6a0e1f3fb414d7c956ff14a34
BryceDMonaco <BryceDmonaco@gmail.com>
2016-05-02 01:22:58 -0700

    Added Project12

---
+++ Project12/Driver.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "inheritance.h"
+
+using namespace std;
+
+int main ()
+{
+	return 0;
+
+}
---
+++ Project12/inheritance.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include "inheritance.h"
+
+#define PI 3.14159265
+
+using namespace std;
+
+//Rectangle Implementation
+//******************************************************************************************
+//Constructor
+Rectangle::Rectangle (float sentLength, float sentWidth)
+{
+	width = sentWidth;
+	length = sentLength;
+
+	isThreeDimensional = false;
+
+}
+
+float Rectangle::calculateAreaOrVolume ()
+{
+	return (length * width);
+
+}
+
+float Rectangle::calculatePerimeterOrSurfaceArea ()
+{
+	return ((length * 2) + (width * 2));
+
+}
+
+void Rectangle::print ()
+{
+	cout << "Rectangle with length: " << length << " and width: " << width;
+
+	return;
+
+}
+
+//Rectangular Prism Implementation
+//******************************************************************************************
+//Constructor
+RectangularPrism::RectangularPrism (float sentLength, float sentWidth, float sentHeight)
+{
+	length = sentLength;
+	width = sentWidth;
+	height = sentHeight;
+
+	isThreeDimensional = true;
+
+}
+
+float RectangularPrism::calculateAreaOrVolume () //Calculates volume
+{
+	return (length * width * height);
+
+}
+
+float RectangularPrism::calculatePerimeterOrSurfaceArea ()
+{
+	int areaOne = length * width;
+	int areaTwo = length * height;
+
+	return ((2 * areaTwo) * (4 * areaOne)); //Total six sides
+
+}
+
+void RectangularPrism::print ()
+{
+	cout << "Rectangular Prism with length: " << length << ", width: " << width << ", and height: " << height;
+
+	return;
+
+}
+
+//Circle Implementation
+//******************************************************************************************
+//Constructor
+Circle::Circle (float sentRadius)
+{
+	radius = sentRadius;
+
+	isThreeDimensional = false;
+
+}
+
+float Circle::calculateAreaOrVolume ()
+{
+	return (PI * (radius * radius)); //A = pi*r^2
+
+}
+
+float Circle::calculatePerimeterOrSurfaceArea ()
+{
+	return (2 * PI * radius);
+
+}
+
+void Circle::print ()
+{
+	cout << "Circle with radius: " << radius;
+
+	return;
+
+}
+
+//Cylinder Implementation
+//******************************************************************************************
+//Constructor
+Cylinder::Cylinder (float sentRadius, float sentHeight)
+{
+	radius =sentRadius;
+	height = sentHeight;
+
+	isThreeDimensional = true;
+
+}
+
+float Cylinder::calculateAreaOrVolume ()
+{
+	return (height * (PI * (radius * radius))); //V = h*(pi*r^2)
+
+}
+
+float Cylinder::calculatePerimeterOrSurfaceArea ()
+{
+	return ((2 * PI * radius * height) + (2 * PI * (radius * radius))); //A = 2pirh+2pir^2
+
+}
+
+void Cylinder::print ()
+{
+	cout << "Cylinder with radius: " << radius << " and height: " << height;
+
+	return;
+
+}
